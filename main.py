23792a81eb08580a70a192170b2d7bb3db9ddaef
greemfox <pavelkurchanov.lpk@gmail.com>
2026-04-25 13:03:59 +0700

    settle on calamine as the backend

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from python_calamine import CalamineWorkbook, SheetVisibleEnum
+from pathlib import Path
+
 # TODO:
-# [ ] Parse the XML
-# [ ] Start talking to Max about it
+# Parse the XML
+# Start talking to Max about it
 
 
 def main():
-    print("Hello from raspinform!")
+    raw_dir = Path(__file__).resolve().parent / "raw"
+    week_dir = raw_dir / input("vn или nn? ").lower().strip()
+    book_path = week_dir / (input("ff, fmf или pip? ").lower().strip() + ".xlsx")
+    book = CalamineWorkbook.from_path(book_path)
+    sheet_names = [
+        sheet.name
+        for sheet in book.sheets_metadata
+        if sheet.visible == SheetVisibleEnum.Visible
+    ]
+    print(*sheet_names)
 
 
 if __name__ == "__main__":
