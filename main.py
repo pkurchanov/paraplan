2b74a670a78b4beb8fa199226725a92fe5a4034e
greemfox <pavelkurchanov.lpk@gmail.com>
2026-04-25 14:01:41 +0700

    hierarchize dat thang

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from python_calamine import CalamineWorkbook, SheetVisibleEnum
 from pathlib import Path
 
-# TODO:
-# Parse the XML
-# Start talking to Max about it
-
 
 def main():
     raw_dir = Path(__file__).resolve().parent / "raw"
-    week_dir = raw_dir / input("vn или nn? ").lower().strip()
-    book_path = week_dir / (input("ff, fmf или pip? ").lower().strip() + ".xlsx")
-    book = CalamineWorkbook.from_path(book_path)
-    sheet_names = [
-        sheet.name
-        for sheet in book.sheets_metadata
-        if sheet.visible == SheetVisibleEnum.Visible
-    ]
-    print(*sheet_names)
+    for week in ["vn", "nn"]:
+        week_dir = raw_dir / week
+        for wb in ["ff", "fmf", "pip"]:
+            book_path = week_dir / (wb + ".xlsx")
+            book = CalamineWorkbook.from_path(book_path)
+            sheet_names = [
+                sheet.name
+                for sheet in book.sheets_metadata
+                if sheet.visible == SheetVisibleEnum.Visible
+            ]
+            print(f"{week.upper() + '-' + wb.upper():>6}:", *sheet_names)
+            sheets = [book.get_sheet_by_name(name) for name in sheet_names]
+            # table[
+            #   date, year, code, workweek
+            # ]
+            # workweek[
+            #   day1..day6
+            # ]
+            # day[
+            #   class1..class4
+            # ]
+            # class[
+            #   reader, subj, form, room
+            # ]
 
 
 if __name__ == "__main__":
