2739da62a654c8ddc65d747627e1e0f3af9b8d82
greemfox <pavelkurchanov.lpk@gmail.com>
2026-04-26 10:09:52 +0700

    tidy things up

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 from python_calamine import CalamineWorkbook, SheetVisibleEnum
 from pathlib import Path
 
+# На подумать: кэширование
+
 
 def wash(string):
     """Помыть строку перед использованием"""
@@ -8,23 +10,23 @@ def wash(string):
 
 
 def kabinyetify(x):
-    """Избегать кабинетов в духе 302.0"""
+    """Избежать кабинетов в духе 302.0"""
     return wash(int(x)) if type(x) is float else wash(x)
 
 
 def main():
     # Каталог исходников
     raw_dir = Path(__file__).resolve().parent / "raw"
-    # Сформированные расписания на неделю
+    # Сформированные недельные расписания
     time_tables = []
     for week_tag in raw_dir.iterdir():
         week_dir = raw_dir / week_tag
         for book_file in week_dir.iterdir():
             book = CalamineWorkbook.from_path(book_file)
             sheet_names = [
-                sheet.name
-                for sheet in book.sheets_metadata
-                if sheet.visible == SheetVisibleEnum.Visible
+                sheetmd.name
+                for sheetmd in book.sheets_metadata
+                if sheetmd.visible == SheetVisibleEnum.Visible
             ]
             sheets = [
                 book.get_sheet_by_name(name).to_python(skip_empty_area=False)
@@ -44,30 +46,34 @@ def main():
                     workday = []
                     for j in range(4):
                         # Между днями по 12 строк, между занятиями по 2
-                        ordinate = 9 + i * 13 + j * 3
-                        abscissa = 2
-                        instructor_maybe = wash(sheet[ordinate][abscissa])
+                        inst_y = 9 + i * 13 + j * 3
+                        inst_x = 2
+                        instructor_maybe = wash(sheet[inst_y][inst_x])
+                        if instructor_maybe == "":
+                            workday.append(None)
+                            break
                         if instructor_maybe == "выходной день":
                             workday.append((instructor_maybe.upper(), "", "", ""))
                             break
                         # Название предмета (включая пометы в скобках)
-                        subject: str = sheet[ordinate - 1][abscissa]  # ty:ignore[invalid-assignment]
+                        subject: str = sheet[inst_y - 1][inst_x]  # ty:ignore[invalid-assignment]
                         # Форма проведения занятия
-                        form = wash(sheet[ordinate + 1][abscissa])
+                        form = wash(sheet[inst_y + 1][inst_x])
                         # Кабинет
                         classroom = ""
                         if form == "асинхронно":
-                            corner_num = kabinyetify(sheet[ordinate + 1][abscissa + 2])
+                            corner_num = kabinyetify(sheet[inst_y + 1][inst_x + 2])
                             if corner_num != "":
                                 classroom = corner_num
                         else:
-                            classroom = kabinyetify(sheet[ordinate][abscissa + 2])
+                            classroom = kabinyetify(sheet[inst_y][inst_x + 2])
                         workday.append(
                             (instructor_maybe.title(), subject, form, classroom)
                         )
                     workweek.append(workday)
                 time_tables.append((date, year, code.upper(), workweek))
     print(*time_tables, sep="\n\n")
+    return time_tables
 
 
 if __name__ == "__main__":
