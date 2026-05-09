1dfa123fe9808d893f0745c95304eff1f3959aaf
greemfox <pavelkurchanov.lpk@gmail.com>
2026-05-09 12:57:37 +0700

    adopt a simpler button-based design for the time being

--- maxbot.py
+++ maxbot.py
@@ -1,37 +1,76 @@
 import asyncio
-from parser import main as parse
+import logging
+from parser import main as load_schedule, wash
 from keys import TEST_TOKEN
 from maxapi import Bot, Dispatcher
+from maxapi.types import MessageCreated, Command, MessageCallback
+from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
+from maxapi.types.attachments.buttons import CallbackButton
 
-# TODO:
-# - Написать и где-то поместить мини-приложение
-# - Скармливать ему: роль (студент/преподаватель), группу (если студент), фамилию (если преподаватель)
-# - Фильтровать таблицы по соответствующему критерию
-# - Получать обратно красивую страничку с расписанием
-
+logging.basicConfig(level=logging.INFO)
 bot = Bot(TEST_TOKEN)
 dp = Dispatcher()
 
-_schedule_cache = None
+schedule = None
+codes = set()
+names = set()
+listening: bool = False
 
 
-def load_schedule():
-    """Загружает и кэширует расписание"""
-    global _schedule_cache
-    if not _schedule_cache:
-        _schedule_cache = parse()
-    return _schedule_cache
+@dp.message_created(Command("rasp"))
+async def start_handler(event: MessageCreated):
+    global schedule
+    global listening
+    try:
+        schedule = load_schedule()
+    except Exception as e:
+        await event.message.answer(text=f"⚠️ Ошибка при загрузке расписаний: {e}")
+    else:
+        await event.message.answer(text="Напишите код группы или ФИО преподавателя")
+        codes.update(t[2] for t in schedule)  # ty:ignore[not-iterable]
+        names.update(
+            name
+            for table in schedule  # ty:ignore[not-iterable]
+            for day in table[3]
+            for lesson in day
+            if (name := lesson[0]) and name not in ("", "ВЫХОДНОЙ ДЕНЬ")
+        )
+        listening = True
+
+
+@dp.message_created()
+async def search_handler(event: MessageCreated):
+    global listening
+    if listening:
+        query = wash(event.message.body.text)  # ty:ignore[unresolved-attribute]
+        keyboard = InlineKeyboardBuilder()
+        keyboard_height = 0
+        for term in codes | names:
+            if query in wash(term):
+                keyboard.row(CallbackButton(text=term, payload=term))
+                keyboard_height += 1
+                if keyboard_height == 5:
+                    break
+        await event.message.answer(text="Найдено:", attachments=[keyboard.as_markup()])
+        listening = False
+
+
+@dp.message_callback()
+async def button_handler(event: MessageCallback):
+    term_clicked = event.callback.payload
+    # TODO: отфильтровать и вывести само расписание
+    if term_clicked in codes:
+        await event.message.edit(  # ty:ignore[unresolved-attribute]
+            text=f"Расписание группы {term_clicked}", attachments=[]
+        )
+    else:
+        await event.message.edit(  # ty:ignore[unresolved-attribute]
+            text=f"Расписание преподавателя {term_clicked}", attachments=[]
+        )
 
 
 async def main():
-    try:
-        load_schedule()
-        print("✅ Расписание загружено")
-    except Exception as e:
-        print(f"⚠️ Ошибка загрузки расписания: {e}")
-        return e
-    finally:
-        await dp.start_polling(bot)
+    await dp.start_polling(bot, skip_updates=True)
 
 
 if __name__ == "__main__":
